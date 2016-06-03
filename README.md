6cf16a9b86d92b4b9133ad57a90608142d6f7285
Ben Parizek <ben@barrelstrengthdesign.com>
2016-06-02 17:53:56 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Sprout Plugins - Translations
+
+Sprout Plugins are designed to be translated into your language of choice. This repository contains the most recent English translation file we know of for each plugin. 
+
+If you translate one of our plugins into another language, please let us know or submit a pull request and we will happily add your translation to the repository.
+
+_Note: We are working on getting a better process in place to make these updates more consistent with the plugin releases._
+
+## The translation files
+
+The English translation file is stored in the `pluginname/translations` folder for each plugin along with any other translations that we know of for the plugin.
+
+## Translating a plugin into your language
+
+In this example we will use Sprout Forms.
+
+1. Copy the translation file from sproutforms/translations/en_us.php
+2. Rename the file to your chosen language: `en_us.php` => `nl.php`
+3. Update the file to include your translations:
+
+- en_us.php: 'Add a form' => 'Add a form',
+- nl.php: 'Add a form' => 'Formulier toevoegen',
+
+4. Copy your new translation file to your `sproutforms/translations` folder.
+
---
+++ sproutforms/translations/nl.php
@@ -0,0 +1,167 @@
+<?php
+return array(
+    'Add a form' =>
+    'Formulier toevoegen',
+
+    'Add a form entry' =>
+    'Formulier inzending toevoegen',
+
+    'Advanced Fields' =>
+    'Geavanceerde velden',
+
+    'All Entries' =>
+    'Alle inzendingen',
+
+    'All Forms' =>
+    'Alle formulieren',
+
+    'Basic Fields' =>
+    'Basis velden',
+
+    'Control Panel' =>
+    'Bedieningspaneel',
+
+    'Couldn’t save form.' =>
+    'Kon formulier niet opslaan.',
+
+    'Couldn’t save settings.' =>
+    'Kon instellingen niet opslaan.',
+
+    'Create a new field' =>
+    'Nieuw veld toevoegen',
+
+    'Create a new form' =>
+    'Nieuw formulier toevoegen',
+
+    "Custom Submit Action URL <span class='info'>If you want to POST your form data to a third party URL, add that URL here. Currently, only fields using the same handle naming convention as Craft are supported.</span>" =>
+    "Aangepast verzend actie URL <span class='info'>Als u uw data naar een derde partij URL wilt versturen, voer deze URL dan hier in. Momenteel zijn alleen velden met dezelfde handle benamingen als Craft ondersteund.</span>",
+
+    'Date Created' =>
+    'Aanmaakdatum',
+
+    'Date Updated' =>
+    'Wijzigingsdatum',
+
+    'Design your form' =>
+    'Stel uw formulier lay-out samen',
+
+    'Display Tab Titles on Forms' =>
+    'Tab titels weergeven bij formulieren',
+
+    "Don't install any example forms or data, I will build the forms I need myself." =>
+    "Installeer geen voorbeeld formulieren of data, ik ga zelf de formulieren aanmaken.",
+
+    'Edit Form Settings' =>
+    'Formulier instellingen wijzigen',
+
+    'Email Recipients <span class="info">All fields can use dynamic values from the submitted form fields. Reference your form fields by wrapping the field name in curly braces.  If your field is <strong><code>name="fields[email]"</code></strong> you would use: <strong><code>{email}</code></strong>.</span>' =>
+    'Email ontvangers <span class="info">Alle velden kunnen gebruik maken van dynamische veld waarden die bij het formulier gesubmit zijn. Refereer naar uw formulier velden door de handle in blokhaken te plaatsen. Als uw veld als volgt heet <strong><code>name="fields[email]"</code></strong>, kunt u dit gebruiken: <strong><code>{email}</code></strong>.</span>',
+
+    'Enter comma seperated email addresses to notify on form submission. Leave blank to disable notifications.' =>
+    'Voer komma gescheiden emailadressen in om deze een notificatie te sturen als het formulier wordt gesubmit. Leeg laten om notificaties uit te schakelen.',
+
+    'Error creating Form' =>
+    'Fout bij het maken van formulier',
+
+    'Examples successfully installed.' =>
+    'Voorbeelden succesvol geïnstalleerd.',
+
+    'Form' =>
+    'Formulier',
+
+    'Forms' =>
+    'Formulieren',
+
+    'Form Name' =>
+    'Formulier naam',
+
+    'Form saved.' =>
+    'Formulier opgeslagen.',
+
+    'Getting Started' =>
+    'Aan de slag',
+
+    "Handle <span class='info'>How you’ll refer to this form in the templates.</span>" =>
+    "Handle <span class='info'>Hoe u zult refereren naar dit formulier in de templates.</span>",
+
+    'If you want more control over the formatting of your templates, Sprout Forms will check in this folder first for any overrides that you want to add.  If none exists, it will fall back to the default templates.' =>
+    'Als u meer controle wilt over de format in uw templates, zal Sprout Forms eerst in deze aangegeven map kijken of er iets overschreven moet worden. Als deze map niet bestaat, of leeg is, zal het terugvallen op de standaard templates.',
+
+    'Install Example Forms' =>
+    'Installeer voorbeeld formulieren',
+
+    'Intuitive, human-readable plugin name for the end user.' =>
+    'Intuïtief, menselijk leesbare plugin naam voor de eindgebruiker.',
+
+    "Name <span class='info'>What this form will be called in the CP.</span>" =>
+    "Naam <span class='info'>Hoe dit formulier zal worden genoemd in de CP.</span>",
+
+    'New Form' =>
+    'Nieuw formulier',
+
+    'No form exists with the handle “{handle}”' =>
+    'Er bestaat geen formulier met handle “{handle}”',
+
+    'No form exists with the ID “{id}”' =>
+    'Er bestaat geen formulier met ID “{id}”',
+
+    'Notifications' =>
+    'Notificaties',
+
+    'Number of Fields' =>
+    'Aantal velden',
+
+    'Plugin Name' =>
+    'Plugin naam',
+
+    "Redirect Page <span class='info'>Where should the user be redirected upon form submission? <i>NOTE: Leaving blank will redirect user back to the form.</i></span>" =>
+    "Redirect pagina <span class='info'>Waar moet de gebruiker naar toe gestuurd worden nadat het
+    formulier gesubmit is? <i>NOTE: Als dit wordt leeg gelaten, zal de gebruiker teruggestuurd worden naar het formulier.</i></span>",
+
+    'Reply To Email Address' =>
+    'Emailadres waar reactie naar toe wordt gestuurd',
+
+    'Select one or more forms.  If none selected, notification will apply to all.' =>
+    'Selecteer een of meerdere formulieren. Als er niets is geselecteerd, krijgt iedereen een
+    notificatie.',
+
+    "Sender Email Address <span class='info'>Consider the deliverability implications before you dynamically change your <strong>Sender Email Address</strong>.</span>" =>
+    "Emailadres afzender <span class='info'>Overweeg de mogelijke levering implicaties voordat u de <strong>Emailadres afzender</strong> vervangt.</span>",
+
+    "Sender Name <span class='info'>Consider the deliverability implications before you dynamically change your <strong>Sender Name</strong>.</span>" =>
+    "Naam afzender <span class='info'>Overweeg de mogelijke levering implicaties voordat u de <strong>Naam afzender</strong> vervangt.</span>",
+
+    'Settings saved.' =>
+    'Instellingen opgeslagen.',
+
+    "Submit Button Text <span class='info'>What the text in the submit button should be.</span>" =>
+    "Verzendknop tekst <span class='info'>Wat de tekst in de verzendknop moet zijn.</span>",
+
+    'Template Folder Override' =>
+    'Template map overschrijven',
+
+    "The 'From' email address of your notification email" =>
+    "Het emailadres 'From' van de afzender in uw notificatie email",
+
+    "The 'Reply To' email address of your notification email" =>
+    "Het emailadres 'Reply To' waar de reactie heen gaat, in uw notificatie email",
+
+    'The name of the sender of your notification email' =>
+    'De naam van de afzender in uw notificatie email',
+
+    'The subject line of your notification email' =>
+    'Het onderwerp van uw notificatie e-mail',
+
+    "Title Format <span class='info'>Entry Titles are auto-generated based on the Title Format you enter here. All information contained in the Title will also be searchable. You may use the same syntax here as you would with Dynamic Titles in your Sections. (i.e. {dateCreated|date('Ymd')} {fullName})</span>" =>
+    "Titel Formaat <span class='info'>Bericht Titels worden automatisch gegenereerd op basis van het Titel Formaat dat u hier invoert. Alle informatie in de titel zal ook vindbaar zijn. U kunt dezelfde syntax gebruiken die ook gebruikt kan worden bij de Dynamische Titels in uw secties. (Bijv. {dateCreated|date('Ymd')} {fullName})</span>",
+
+    'Total Entries' =>
+    'Aantal inzendingen',
+
+    'Unable to install the examples.' =>
+    'Kon de voorbeelden niet installeren.',
+
+    'Which group should this form be displayed in?' =>
+    'In welke group moet dit formulier weergegeven worden?',
+
+);
